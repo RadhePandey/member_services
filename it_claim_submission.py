86b91541117b0f2ad604b81cd14dea1a3cb8225d
Radhe <radhepandey08@gmail.com>
2026-09-14 17:58:30 +0530

    first commit

--- it_claim_submission.py
+++ it_claim_submission.py
@@ -11,5 +11,7 @@ class IT_Claim_Submission:
         self.add_claim_CTA = page.locator('(//button[@class="underline  px-4 py-2 text-sm flex items-center hover:opacity-90"])[1]')
 
         self.search_member_CTA = page.locator('//input[@placeholder="Search by Name or IC number..."]')
+
+        self.search_member_CTA = page.locator('//input[@placeholder="Search by Name or IC number..."]')
         #here we will type user name : Deepanshu Hooda and select user and user redirect to claim form 
         
